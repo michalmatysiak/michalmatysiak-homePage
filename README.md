b5e25b42988548c2f9099801d82ae0e331f16100
Michał Matysiak <michalmatysiak02@gmail.com>
2019-03-18 21:59:22 +0100

    Complete project

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# michalmatysiak.github.io
+Responsywna strona typu "one page" o mnie i o moich projektach. 
+Rozwiązania, technologie i narzędzia:
+
+   * HTML 5
+   * CSS 3
+   * Sass
+   * RWD
+   * JavaScript
+   * Bootstrap 4 (grid)
+   * npm (task runner)
+
+<a href="https://michalmatysiak.github.io/">Home page Michał</a>
---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*---------------------------HAMBURGER MENU----------------------*/
+
+const menuIcon = document.querySelector('.nav-icon');
+const menuLinks = document.querySelector('.nav-links');
+const nav = document.querySelector('nav');
+const introduction = document.querySelector('.introduction-box');
+const arrow = document.querySelector('.arrow-box');
+
+let toggleMenu = () => {
+  introduction.classList.toggle('hide');
+  arrow.classList.toggle('hide');
+  menuIcon.classList.toggle('open');
+  menuLinks.classList.toggle('show-links');
+  nav.classList.toggle('nav-collapse');
+};
+
+menuIcon.addEventListener('click', toggleMenu);
+menuLinks.addEventListener('click', toggleMenu);
